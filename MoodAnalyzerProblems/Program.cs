6aa06c59b239df1dc58f3d82594c3db3d054163f
Rakesh-space <rakeshsmusale03@gmail.com>
2021-11-11 13:57:26 +0530

    Welcome message for MoodAnalyzer

---
+++ MoodAnalyzerProblems/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoodAnalyzerProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Welcome to MoodAnalyzer Problem..");
+        }
+    }
+}
