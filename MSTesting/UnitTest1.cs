75f75f9d1262e0913905a063cae2884debf6226f
Rakesh-space <rakeshsmusale03@gmail.com>
2021-11-11 14:49:57 +0530

    UC3 Custom Exception

--- MSTesting/UnitTest1.cs
+++ MSTesting/UnitTest1.cs
@@ -1,18 +1,19 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MoodAnalyzerProblems;
 
+
 namespace MSTesting
 {
     [TestClass]
     public class UnitTest1  //here performed the unit testing for class
     {
         [TestMethod]
-        public void AnalyseHappyMood() //here performed the unit testing for method
+        public void AnalyseHappyMood()  //here performed the unit testing for method
         {
-        
+            // Test Case 1
             //Arrange
-            string message = "I am in sad mood";
-            string expected = "SAD";
+            string message = null;
+            string expected = "happy";
 
             //Act
             ModeAnalyzer modeAnalyzer = new ModeAnalyzer(message);
@@ -20,6 +21,28 @@ namespace MSTesting
 
             //Assert
             Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void getCustomNullException()
+        {
+            //Arrange
+            string expected = "Message Should Not Be Null";
+            ModeAnalyzer modeAnalyzer = new ModeAnalyzer(null);
+
+            try
+            {
+                //Act
+                string actual = modeAnalyzer.AnalyzeMood();
+
+            }
+            catch (CustomException ex)
+            {
+
+                //Assart
+                Assert.AreEqual(expected, ex.Message);
+            }
 
         }
     }
